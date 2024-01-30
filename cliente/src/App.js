2ce47b2cf4b48e8363db5e13adf1d98d433a9a67
abaduna~ <artuelrey@gmail.com>
2024-01-30 14:46:24 -0300

    falta room

---
+++ cliente/src/App.js
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import io from "socket.io-client";
+import Button from "react-bootstrap/Button";
+import { Row, Col } from "react-bootstrap";
+function App() {
+  const socket = io("http://localhost:3001");
+  const [select, setSelct] = useState(null);
+  const [id, setId] = useState(null);
+  const [resultado, setResultado] = useState(null);
+
+  let data = {
+    select,
+    navegadorId: id,
+  };
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(`conection ${socket.id}`);
+      setId(socket.id);
+    });
+    socket.on("mensajeServidor", (data) => {
+      console.log("Mensaje del servidor:", data);
+      setResultado(data);
+    });
+  }, []);
+
+  const elejir = () => {
+    console.log(data);
+    socket.emit("play", data);
+    console.log(`id ${data.customId}`);
+  };
+  return (
+    <div>
+      <h2 className="App">resultado {resultado}</h2>
+      <h2 className="App">{select}</h2>
+      <div>
+        <Row>
+          <Col md={4}>
+            <div className="d-grid gap-2">
+              <Button className=" btn-primary" onClick={() => setSelct(1)}>
+                piedra
+              </Button>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="d-grid gap-2">
+              <Button  variant="warning"  onClick={() => setSelct(2)}>papel</Button>
+            </div>
+          </Col>
+          <Col md={4}>
+            <div className="d-grid gap-2">
+              <Button  variant="success"  onClick={() => setSelct(3)}>tijera</Button>
+            </div>
+          </Col>
+        </Row>
+      </div>
+      <div className="d-grid gap-2 mt-2">
+        <Button variant="info"  onClick={elejir}>Elejir numero</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ server/index.js
@@ -0,0 +1,146 @@
+const http = require("http");
+const fs = require("fs");
+
+const server = http.createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+  },
+});
+let jugadas = [];
+let jugadorUno = null;
+let jugadorDos = null;
+let idJugadoUno = null
+let idJugadoDos = null
+const ganar = () => {
+  if (jugadorUno === 1) {
+    if (jugadorDos === 1) {
+      console.log(`empate`);
+      io.to(idJugadoUno).emit(
+        "mensajeServidor",
+        "empate"
+      );
+      io.to(idJugadoDos).emit(
+        "mensajeServidor",
+        "empate"
+      );
+    }
+    if (jugadorDos === 2) {
+      console.log(`gano juagador dos`);
+      io.to(idJugadoUno).emit(
+        "mensajeServidor",
+        "perdiste"
+      );
+      io.to(idJugadoDos).emit(
+        "mensajeServidor",
+        "ganaste"
+      );
+    }
+    if (jugadorDos === 3) {
+      console.log(`gano juagador uno`);
+      io.to(idJugadoUno).emit(
+        "mensajeServidor",
+        "ganaste"
+      );
+      io.to(idJugadoDos).emit(
+        "mensajeServidor",
+        "perdiste"
+      );
+    }
+  }
+  if (jugadorUno === 2) {
+    if (jugadorDos === 1) {
+      console.log(`gana jugador 1`);
+      io.to(idJugadoUno).emit(
+        "mensajeServidor",
+        "ganaste"
+      );
+      io.to(idJugadoDos).emit(
+        "mensajeServidor",
+        "perdiste"
+      );
+    }
+    if (jugadorDos === 2) {
+      console.log(`empate`);
+      io.to(idJugadoUno).emit(
+        "mensajeServidor",
+        "empate"
+      );
+      io.to(idJugadoDos).emit(
+        "mensajeServidor",
+        "empate"
+      );
+    }
+    if (jugadorDos === 3) {
+      console.log(`gano juagador dos`);
+      io.to(idJugadoUno).emit(
+        "mensajeServidor",
+        "perdiste"
+      );
+      io.to(idJugadoDos).emit(
+        "mensajeServidor",
+        "ganaste"
+      );
+    }
+  }
+  if (jugadorUno === 3) {
+    if (jugadorDos === 1) {
+      console.log(`gana jugador 2`);
+      io.to(idJugadoUno).emit(
+        "mensajeServidor",
+        "perdiste"
+      );
+      io.to(idJugadoDos).emit(
+        "mensajeServidor",
+        "ganaste"
+      );
+    }
+    if (jugadorDos === 2) {
+      console.log(`gana jugador 1`);
+      io.to(idJugadoUno).emit(
+        "mensajeServidor",
+        "ganaste"
+      );
+      io.to(idJugadoDos).emit(
+        "mensajeServidor",
+        "perdiste"
+      );
+    }
+    if (jugadorDos === 3) {
+      console.log(`empate`);
+      io.to(idJugadoUno).emit(
+        "mensajeServidor",
+        "empate"
+      );
+      io.to(idJugadoDos).emit(
+        "mensajeServidor",
+        "empate"
+      );
+    }
+  }
+  jugadas = [];
+};
+io.on("connection", (socket) => {
+  console.log(`connection`);
+  socket.on("play", (data) => {
+    console.log(`data`);
+    console.log(data);
+    jugadas.push(data);
+    console.log(`jugadas`);
+    console.log(jugadas);
+    jugadorUno = jugadas[0].select;
+    idJugadoUno = jugadas[0].navegadorId
+    console.log(`jugadorUno ${jugadorUno}`);
+
+    if (jugadas[1]) {
+      jugadorDos = jugadas[1].select;
+      idJugadoDos = jugadas[1].navegadorId
+      console.log(`jugadorDos ${jugadorDos}`);
+      ganar();
+    }
+    let clientId = data.navegadorId;
+    
+  });
+});
+
+server.listen(3001);
