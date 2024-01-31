587b476145be7b6cd80633f32ea4308925e129f7
abaduna~ <artuelrey@gmail.com>
2024-01-31 13:07:36 -0300

    falta individual

--- cliente/src/router/Router.js
+++ cliente/src/router/Router.js
@@ -5,14 +5,18 @@ import {
   Navigate,
 } from "react-router-dom";
 import Multijugador from "../pages/multijugador";
-import Home from "../pages/salas"
+import Sala from "../pages/Salas";
+import Home from "../pages/Home";
+import Individual from "../pages/Individual";
 const RouterPrincipal = () => {
   return (
     <Router>
       <Routes>
-      <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/sala" element={<Sala />} />
         <Route path="/room/:roomState" element={<Multijugador />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
+        <Route path="/individual" element={<Individual />} />
+        {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
       </Routes>
     </Router>
   );
